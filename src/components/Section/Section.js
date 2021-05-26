76e7e3a4aab5f7b5d0f7fa1a6f242e10218a1b42
alexherlanda <alexisherlanda.mx@gmail.com>
2021-05-25 21:14:31 -0500

    feat: Desing section component

--- src/components/Section/Section.js
+++ src/components/Section/Section.js
@@ -1,11 +1,15 @@
 import React from "react";
+import "./Section.css";
 
 const Section = ({ title, subtitle, description, children }) => {
   return (
-    <section>
-      <h2> {title} </h2>
-      <h3> {subtitle && subtitle.toUpperCase()} </h3>
-      <p> {description} </p>
+    <section className="section">
+      <h2 className="section__title"> {title} </h2>
+      {typeof subtitle === "string" ? (
+        <h3 className="section__subtitle"> {subtitle.toUpperCase()} </h3>
+      ) : null}
+
+      <p className="section__description"> {description} </p>
       {children}
     </section>
   );
--- src/containers/GeneralInfo/GeneralInfo.js
+++ src/containers/GeneralInfo/GeneralInfo.js
@@ -6,7 +6,7 @@ const GeneralInfo = () => {
     <div>
       <Section
         title="Sumante al dialogo"
-        description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam ex erat, maximus sit amet viverra sed, pharetra at ex. Phasellus a augue eros. Mauris ac mauris lorem. Quisque at lectus purus. Aenean vel ipsum venenatis velit tristique ultrices. Donec pretium lacinia justo id venenatis. "
+        description="Lorem ipsum dolor sit amet, consectetur adipiscing elit "
       >
         Componente de ganadores anteriores
       </Section>
@@ -14,7 +14,7 @@ const GeneralInfo = () => {
       <Section
         title="Camino al torneo"
         subtitle="Mx Debate 2021"
-        description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam ex erat, maximus sit amet viverra sed, pharetra at ex. Phasellus a augue eros. Mauris ac mauris lorem. Quisque at lectus purus. Aenean vel ipsum venenatis velit tristique ultrices. Donec pretium lacinia justo id venenatis. "
+        description="Lorem ipsum dolor sit amet, consectetur adipiscing elit "
       >
         Componente de pasos
       </Section>
