59470d98485a4dab112fdfd44aa037160ba55d36
alexherlanda <alexis.herlanda@autozone.com>
2021-05-29 15:04:14 -0500

    feat(Schedule): Creates basic functionality for shedule width data models

--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,6 @@
 import Section from './Section';
 import Tabs from './Tabs';
+import MediaCard from './MediaCard';
+import ActivitiesList from './ActivitiesList';
 
-export { Tabs, Section };
+export { ActivitiesList, Tabs, Section, MediaCard };
