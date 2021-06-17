f1c71381622756820a866c040dd9ee70edb75285
Alexis Herlanda <alexisherlanda@autozone.com>
2021-06-16 20:12:27 -0500

    fiix(deploy) hides sectiont and configures index for deploy

--- src/data/sections.js
+++ src/data/sections.js
@@ -1,8 +1,6 @@
 const sections = [
-  { title: 'Información general', to: '/' },
   { title: 'Horario', to: '/schedule' },
   { title: 'Materiales', to: '/education' },
-  { title: 'En vivo', to: '/live' },
   { title: 'Mi sitio', to: '/my-tournament' },
 ];
 
